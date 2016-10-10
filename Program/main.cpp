271cc23d3cdb9f8701b5c40df237691bcd3dd3c2
Boyang Yan <yanboyang713@gmail.com>
2016-10-10 18:45:52 +1100

    checkRunformate

--- Program/main.cpp
+++ Program/main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 using namespace std;
-
+bool checkRunProgramFormat(int);
 int main(int argc, char *argv[])
 {
-	
-	return;
+	if (!checkRunProgramFormat(argc))
+		return 0;
+	return 0;
+}
+bool checkRunProgramFormat(int argc)
+{
+	if (argc != 4)
+	{
+		cout << "Run program method is wrong!!!" << endl;
+		cout << "run format: Traffic Vehicles.txt Stats.txt Days" << endl;
+		return false;
+	}
+	return true;
 }
